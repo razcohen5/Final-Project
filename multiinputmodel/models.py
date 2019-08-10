e533f612bffed0a7ee09973dd6fd7ae0abb55387
razcohen5 <33660332+razcohen5@users.noreply.github.com>
2019-08-10 16:47:07 +0300

    Add files via upload

---
+++ multiinputmodel/models.py
@@ -0,0 +1,125 @@
+# import the necessary packages
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Dropout
+from keras.layers.core import Dense
+from keras.layers import Flatten
+from keras.layers import Input
+from keras.models import Model
+from keras import backend as K
+
+def create_mlp(dim, regress=False):
+	# define our MLP network
+	model = Sequential()
+	model.add(Dense(8, input_dim=dim, activation="relu"))
+	model.add(Dense(4, activation="relu"))
+
+	# check to see if the regression node should be added
+	if regress:
+		model.add(Dense(1, activation="linear"))
+
+	# return our model
+	return model
+
+def create_cnn(width, height, depth, filters=(16, 32, 64), regress=False):
+	# initialize the input shape and channel dimension, assuming
+	# TensorFlow/channels-last ordering
+	inputShape = (height, width, depth)
+	chanDim = -1
+
+	# define the model input
+	inputs = Input(shape=inputShape)
+
+	# loop over the number of filters
+	for (i, f) in enumerate(filters):
+		# if this is the first CONV layer then set the input
+		# appropriately
+		if i == 0:
+			x = inputs
+
+		# CONV => RELU => BN => POOL
+		x = Conv2D(f, (3, 3), padding="same")(x)
+		x = Activation("relu")(x)
+		x = BatchNormalization(axis=chanDim)(x)
+		x = MaxPooling2D(pool_size=(2, 2))(x)
+
+	# flatten the volume, then FC => RELU => BN => DROPOUT
+	x = Flatten()(x)
+	x = Dense(16)(x)
+	x = Activation("relu")(x)
+	x = BatchNormalization(axis=chanDim)(x)
+	x = Dropout(0.5)(x)
+
+	# apply another FC layer, this one to match the number of nodes
+	# coming out of the MLP
+	x = Dense(4)(x)
+	x = Activation("relu")(x)
+
+	# check to see if the regression node should be added
+	if regress:
+		x = Dense(1, activation="linear")(x)
+
+	# construct the CNN
+	model = Model(inputs, x)
+
+	# return the CNN
+	return model
+
+
+
+
+class SmallerVGGNet:
+	@staticmethod
+	def build(width, height, depth, classes, finalAct="softmax"):
+		# initialize the model along with the input shape to be
+		# "channels last" and the channels dimension itself
+		model = Sequential()
+		inputShape = (height, width, depth)
+		chanDim = -1
+
+		# if we are using "channels first", update the input shape
+		# and channels dimension
+		if K.image_data_format() == "channels_first":
+			inputShape = (depth, height, width)
+			chanDim = 1
+
+		# CONV => RELU => POOL
+		model.add(Conv2D(32, (3, 3), padding="same",
+			input_shape=inputShape))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(MaxPooling2D(pool_size=(3, 3)))
+		model.add(Dropout(0.25))
+
+		# (CONV => RELU) * 2 => POOL
+		model.add(Conv2D(64, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(Conv2D(64, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(MaxPooling2D(pool_size=(2, 2)))
+		model.add(Dropout(0.25))
+
+		# (CONV => RELU) * 2 => POOL
+		model.add(Conv2D(128, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(Conv2D(128, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(MaxPooling2D(pool_size=(2, 2)))
+		model.add(Dropout(0.25))
+
+		# first (and only) set of FC => RELU layers
+		model.add(Flatten())
+		model.add(Dense(1024))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization())
+		model.add(Dropout(0.5))
+
+		# return the constructed network architecture
+		return model
